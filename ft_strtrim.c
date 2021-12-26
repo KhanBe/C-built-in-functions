a9c34f383fab0ced74dad2e733958e11022d5d0f
KhanBe <wistm6980@gmail.com>
2021-12-26 19:12:20 +0900

    mod strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jaewoo <jaewoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 12:20:31 by jaewoo            #+#    #+#             */
-/*   Updated: 2021/11/29 15:42:00 by jaewoo           ###   ########.fr       */
+/*   Updated: 2021/12/26 19:11:47 by jaewoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ char	*ft_strtrim(const char *s1, const char *set)
 	size_t	left;
 	char	*result;
 
-	left = 0;
 	if (s1 == NULL || set == NULL)
 		return (0);
+	left = 0;
 	right = ft_strlen(s1);
 	while (s1[left] && ft_strchr(set, s1[left]))
 		left++;
